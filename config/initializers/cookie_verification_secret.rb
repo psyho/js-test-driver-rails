c79c28b64cc7977909fd3f27418d32314b307185
Adam Pohorecki <adam.pohorecki@llp.pl>
2010-09-01 16:56:41 +0200

    Version bump to 0.0.0

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'de3d1803af7ab041a05bfc952ce9117b137b16e83c2c3aa4d3077111dbb0b359a8b8bc08dfb08cfb79101775569f0be5e30f5e30c4e70f91feeb1ee711065306';
--- config/js_test_driver.rb
+++
@@ -1,7 +0,0 @@
-# use default server port and host
-
-includes 'public/javascripts/greeter.js'
-includes 'test/js/*.js'
-
-browser 'firefox'
-browser 'google-chrome'
