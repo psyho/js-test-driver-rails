a980c2be0dd16945502e4e68f61fe37b9102e837
Adam Pohorecki <adam.pohorecki@llp.pl>
2011-06-08 01:28:52 +0200

    Rake tasks now use Application

--- lib/js_test_driver/tasks.rb
+++ lib/js_test_driver/tasks.rb
@@ -4,25 +4,30 @@ namespace :js_test_driver do
 
   desc "Starts the server using the provided configuration variables"
   task :start_server do
-    JsTestDriver::Runner.new.start_server
+    JsTestDriver::Application.new.start_server
   end
 
   desc "Runs the javascript tests"
   task :run_tests do
-    exit(1) unless JsTestDriver::Runner.new.run_tests(ENV['TESTS'])
+    exit(1) unless JsTestDriver::Application.new.run_tests(:tests => ENV['TESTS'])
   end
 
   desc "Capture the browsers defined in config"
   task :capture_browsers do
-    JsTestDriver::Runner.new.capture_browsers(ENV['BROWSERS'])
+    JsTestDriver::Application.new.capture_browsers(:browsers => ENV['BROWSERS'])
   end
 
   desc "Starts the server, captures the browsers, runs the tests - all at the same time"
   task :run do
-    config = JsTestDriver::Runner.new
+    app = JsTestDriver::Application.new
+
     output_path = ENV['OUTPUT_PATH']
-    output_path = File.join(config.generated_files_dir, 'tests') if ENV['OUTPUT_XML']
-    config.start_server_capture_and_run(ENV['TESTS'], ENV['BROWSERS'], output_path, ENV['CAPTURE_CONSOLE'])
+    output_path = File.join(app.config.generated_files_dir, 'tests') if ENV['OUTPUT_XML']
+
+    exit(1) unless app.run(:tests => ENV['TESTS'],
+                           :browsers => ENV['BROWSERS'],
+                           :output_xml_path => output_path,
+                           :capture_console => ENV['CAPTURE_CONSOLE'])
   end
 
 end
